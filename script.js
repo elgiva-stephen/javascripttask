1ffb58448c79282a3484a3abff65bdf49f0492ce
Elgiva Stephen <elgiva.stephen@gmail.com>
2020-03-27 18:20:50 +0100

    Add javascript task to repository

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let fullName = 'Elgiva Stephen';
+let courses = ['PHP', 'C#', 'Javascript', 'CSS', 'HTML'];
+
+
+function person(fullName, courses) {
+    console.log('Name: ' + fullName + ' ' + 'Courses:' + ' ' + courses);
+}
+person('Elgiva Stephen', ['PHP', 'C#', 'Javascript', 'CSS', 'HTML']);
+
+
+// Courses enrolled for is 5 which is an odd number
+for (let i = 5; i <= 200; i++) {
+    if (i % 2 !== 0) console.log(i);
+    }
+
